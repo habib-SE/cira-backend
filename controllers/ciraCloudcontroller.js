7c70cc980f5b5365c7020bf8c4b20ed0d1fe3644
habib-SE <hab67603@gmail.com>
2026-02-12 00:24:43 +0500

    update link

--- controllers/ciraCloudcontroller.js
+++ controllers/ciraCloudcontroller.js
@@ -74,7 +74,7 @@ export const saveFileToCloud = async (fileBuffer, contentType, originalName) => 
 
     // ✅ FIX: return correct URL path => /cira-cloud/
     const backendUrl =
-      process.env.BACKEND_URL || `http://localhost:${process.env.PORT || 5000}`;
+      process.env.FRONTEND_URL || `http://localhost:${process.env.PORT || 5000}`;
 
     const fileUrl = `${backendUrl}/cira-cloud/${fileName}`;
     return fileUrl;
